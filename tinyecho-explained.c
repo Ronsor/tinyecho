2c90768e6512a367b54c816042809dcf5ffe881b
Ronsor <ronsor@ronsor.pw>
2019-04-10 19:27:55 -0400

    Create tinyecho-explained.c

---
+++ tinyecho-explained.c
@@ -0,0 +1,14 @@
+// Omitted for brevity
+//#include <stdio.h>
+// Main function
+// n: argc: number of arguments (unused)
+// q: argv: arguments
+main(int n,char**q){
+	// increase starting index in arguments (starting from 0 to 1 -- the first non program name argument)
+	// check if the argument is not NULL (which indicates the end)
+	while(*++q)
+		// print the argument, and if the next argument is NULL, a new line, otherwise print a space
+		printf("%s%c",*q,q[1]?' ':'\n');
+	// Implicit
+	//return 0;
+}
