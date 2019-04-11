9d9552d3f31a323c216faef55d648e55cd28e034
Ronsor <ronsor@ronsor.pw>
2019-04-11 16:51:02 -0400

    Update tinyecho-explained.c

--- tinyecho-explained.c
+++ tinyecho-explained.c
@@ -1,11 +1,10 @@
 // Omitted for brevity
-//#include <stdio.h>
+/* #include <stdio.h> */
 // Main function
 // n: argc: number of arguments (unused)
 // q: argv: arguments
 // Implicit
-//int main(int n, char **q) {
-main(int n,char**q){
+/* int */ main(int n,char**q){
 	// increase starting index in arguments (starting from 0 to 1 -- the first non program name argument)
 	// check if the argument is not NULL (which indicates the end)
 	while(*++q)
@@ -13,5 +12,5 @@ main(int n,char**q){
 		// '\n' = 10, ' ' = 32
 		printf("%s%c",*q,q[1]?32:10);
 	// Implicit
-	//return 0;
+	/* return 0; */
 }
